e16b6ad7b4159c96814e884d18c99a8ab338536f
Matthew Griffiths <mg@metalayer.com>
2012-04-04 14:08:48 +0300

    Added new localtagging action

---
+++ actions/lib/localtagging/action.py
@@ -0,0 +1,82 @@
+from metalayercore.actions.classes import BaseAction
+import nltk
+from metalayercore.actions.controllers import ActionController
+
+tokenizer = None
+tagger = None
+
+class Action(BaseAction):
+    def get_unconfigured_config(self):
+        return {
+            'name':'localtagging',
+            'display_name_short':'Tagging',
+            'display_name_long':'Tagging',
+            'image_small':'/static/images/thedashboard/actions/tagging_small.png',
+            'instructions':'This actions does not need configuring.',
+            'configured':True,
+            'elements':[],
+            'content_properties':{
+                'added':[
+                    {
+                        'display_name':'Tags',
+                        'name':'tags',
+                        'type':'string'
+                    }
+                ]
+            }
+        }
+
+    def run(self, config, content):
+        """
+        from metalayercore.actions.controllers import ActionController
+        tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+|[^\w\s]+')
+        tagger = ActionController.CacheGet('localtagging_tagger', -1)
+        if tagger == -1:
+            tagger = nltk.UnigramTagger(nltk.corpus.brown.tagged_sents())
+            ActionController.CacheAdd('localtagging_tagger', tagger)
+        """
+        global tagger, tokenizer
+        if not tokenizer:
+            tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+|[^\w\s]+')
+        if not tagger:
+            tagger = nltk.UnigramTagger(nltk.corpus.brown.tagged_sents())
+        results = [{'id':i['id'], 'tags':self._get_tags(config, i, tokenizer, tagger)} for i in content]
+        return results
+
+    def get_content_item_template(self):
+        config = self.get_unconfigured_config()
+        controller = ActionController(config)
+        sentiment_property = config['content_properties']['added'][0]
+        return ""\
+               "{{if " + controller._search_encode_property(sentiment_property) + "}}"\
+               "    <li class='action_values tags'>"\
+               "        <label><img src='" + config['image_small'] + "' style='position:relative;top:5px;left:-2px;width:16px;height:16px;'/>&nbsp;Tags:</label>&nbsp;"\
+               "        <span style='font-weight:bold;'>" \
+               "            {{each(index, element) " + controller._search_encode_property(sentiment_property) + "}}" \
+               "                {{if element != '_none' && index < 5}}${element}{{/if}} {{if index == 6}}...{{/if}}" \
+               "            {{/each}}" \
+               "        </span>"\
+               "    </li>"\
+               "{{/if}}"
+
+    def _get_tags(self, config, content_item, tokenizer, tagger):
+        text = self._extract_text(config, content_item)
+        tokenized = tokenizer.tokenize(text)
+        tagged = tagger.tag(tokenized)
+        results = []
+        for pair in tagged:
+            if len(pair) > 1:
+                if pair[1] and pair[1].startswith('N'):
+                    if pair[0].lower() not in results and len(pair[0]) > 3:
+                        results.append(pair[0].lower())
+        results = sorted(results, key=lambda r: len(r), reverse=True)
+        return results
+
+    def _extract_text(self, config, content_item):
+        text = ''
+        if 'title' in content_item:
+            text += ' ' + content_item['title']
+        if 'text' in content_item:
+            for t in content_item['text']:
+                text += ' ' + t
+        return text.encode('ascii', 'ignore')
